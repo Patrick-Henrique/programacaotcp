31754db23c21d2d5f5c3be3f86bd8264195b743a
Patrick-Henrique <patrick.bombassaro@catolicasc.org.br>
2020-06-07 05:55:24 +0000

    Início do cliente socket

---
+++ client.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[]){
+    int socket_desc;
+
+    //AF_INET - IPv4, SOCK_STREAM - tcp,0 - IP
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(socket_desc == -1){
+        printf("Não foi possível criar o socket");
+        return 1;
+    }
+
+
+    return 0;
+}
