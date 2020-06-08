54eb1d0f353eff4d8d2819b4418ce108f261aba5
Patrick-Henrique <patrick.bombassaro@catolicasc.org.br>
2020-06-08 08:16:01 +0000

    Servidor recebendo conexão do cliente

--- client.c
+++ client.c
@@ -17,9 +17,9 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    server.sin_addr.s_addr = inet_addr("172.217.28.132"); //converte IP para long
+    server.sin_addr.s_addr = inet_addr("127.0.0.1"); //converte IP para long
     server.sin_family = AF_INET;   //IPv4
-    server.sin_port = htons(80);
+    server.sin_port = htons(8888);
 
     if(connect(socket_desc, (struct sockaddr *) &server, sizeof(server))<0){
         printf("Erro ao conectar-se\n");
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]){
     printf("Conectado.\n");
 
     //envia dados
-    message = "GET / HTTP/1.1\r\n\r\n";
+    message = "Olá Mundo!";
     if(send(socket_desc, message, strlen(message), 0) <0){
         printf("Erro ao enviar\n");
         return 1;
--- server.c
+++ server.c
@@ -6,8 +6,8 @@
 #include <unistd.h> //close
 
 int main(int argc, char *argv[]){
-    int socket_desc;
-    struct sockaddr_in server;
+    int socket_desc, c, new_socket;
+    struct sockaddr_in server, client;
 
     //abre um socket
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    //ojde quero ouvir
+    //onde quero ouvir
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY; //escute em qualquer IP da máquina
     server.sin_port = htons(8888);
@@ -29,6 +29,24 @@ int main(int argc, char *argv[]){
     }
 
     printf("Bind efetuado.\n");
+
+    //escutar
+    listen(socket_desc, 3);
+
+    //aceitar conexões entrantes
+    printf("Aguardando conexões...\n");
+    c = sizeof(struct sockaddr_in);
+    new_socket = accept(socket_desc, (struct sockaddr *) &client, (socklen_t*) &c);
+    if(new_socket < 0){
+        printf("Erro ao aceitar conexão\n");
+        return 1;
+    }
+
+
+    char *client_ip = inet_ntoa(client.sin_addr);
+    int client_port = ntohs(client.sin_port);
+
+        printf("Conexão aceita do cliente %s:%d\n", client_ip, client_port);
 
     return 0;
 }
