29246cc2a3d1f9380ede4c5f2a16a77e7c3cd92b
Patrick-Henrique <patrick.bombassaro@catolicasc.org.br>
2020-06-08 00:25:55 +0000

    Conectar-se ao servidor

--- client.c
+++ client.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
 
 int main(int argc, char *argv[]){
     int socket_desc;
+    struct sockaddr_in server;
 
     //AF_INET - IPv4, SOCK_STREAM - tcp,0 - IP
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -12,6 +14,15 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    server.sin_addr.s_addr = inet_addr("172.217.28.132"); //converte IP para long
+    server.sin_family = AF_INET;   //IPv4
+    server.sin_port = htons(80);
+
+    if(connect(socket_desc, (struct sockaddr *) &server, sizeof(server))<0){
+        printf("Erro ao conectar-se\n");
+        return 1;
+    }
+    printf("Conectado.\n");
 
     return 0;
 }
