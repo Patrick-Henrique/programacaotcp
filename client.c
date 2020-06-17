6d6ff5a6da2399f62e4770fe8457efdc9833f945
Patrick-Henrique <patrick.bombassaro@catolicasc.org.br>
2020-06-17 00:27:35 +0000

    Final

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h> //strlen
+#include <strings.h>
 #include <sys/socket.h>
 #include <arpa/inet.h> //inet_addr
 #include <unistd.h> //close
@@ -7,7 +8,7 @@
 int main(int argc, char *argv[]){
     int socket_desc;
     struct sockaddr_in server;
-    char *message, server_reply[2000];
+    char message[2000], server_reply[2000];
 
     //AF_INET - IPv4, SOCK_STREAM - tcp,0 - IP
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -21,20 +22,37 @@ int main(int argc, char *argv[]){
     server.sin_family = AF_INET;   //IPv4
     server.sin_port = htons(8888);
 
+    //conecta-se ao servidor
     if(connect(socket_desc, (struct sockaddr *) &server, sizeof(server))<0){
         printf("Erro ao conectar-se\n");
         return 1;
     }
     printf("Conectado.\n");
 
-    //envia dados
-    message = "Olá, Mundo!";
-    if(send(socket_desc, message, strlen(message), 0) <0){
+
+
+
+    do {
+        bzero(message, sizeof(message)); //comando para limpar variável - bzero()
+
+        //enviando mensagem
+        printf("Digite uma mensagem: ");
+
+        int ch, n;
+        n=0;
+
+        while((ch= getchar()) != '\n' && n < 2000){
+            message[n]= ch;
+            ++n;
+        }
+
+        //confirma envio dados
+        if(send(socket_desc, message, strlen(message), 0) <0){
         printf("Erro ao enviar\n");
         return 1;
     }
     printf("Dados enviados.\n");
-
+    
     //recebe dados do servidor
     if(recv(socket_desc, server_reply, 2000, 0)<0){
         printf("Falha no recv\n");
@@ -42,6 +60,9 @@ int main(int argc, char *argv[]){
     }
     printf("Resposta recebida.\n");
     printf("%s\n", server_reply);
+    
+    bzero(server_reply, sizeof(server_reply));
+    }while (strcmp(message, "exit") != 0);
 
     //encerra a conexão
     close(socket_desc);
